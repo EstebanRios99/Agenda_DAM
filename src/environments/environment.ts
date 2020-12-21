e4fd19502dbf399cb744551ecbc5a2447ee77cf3
MARVIN ANIBAL ZAMBRANO GILER <marvin.zambrano@epn.edu.ec>
2020-12-21 00:12:47 -0500

    Splash Screen e Informe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,12 @@ export const environment = {
   firebase : {
     apiKey: "AIzaSyBCqrmwFWOnLxnGUI5_sjsaPquFyRsgBGc",
     authDomain: "crud2020b-97f68.firebaseapp.com",
+    databaseURL: "https://crud2020b-97f68-default-rtdb.firebaseio.com",
     projectId: "crud2020b-97f68",
     storageBucket: "crud2020b-97f68.appspot.com",
     messagingSenderId: "997792238375",
-    appId: "1:997792238375:web:208098138fa8a51114b702",
-    measurementId: "G-SJF7324KMJ"
+    appId: "1:997792238375:web:f082473f74400d1814b702",
+    measurementId: "G-7YMM5PSVBZ"
   }
 };
 
